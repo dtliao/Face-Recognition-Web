39af56f3f5d35c3d6c0a7c4f6b1a09d3f1e48108
Daniel Liao <39768672+dtliao@users.noreply.github.com>
2018-06-05 01:25:12 +0800

    update face recognition project

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+
+		if (isSignedIn) {
+			return (
+				<nav className='tr'>
+					<p onClick={() => onRouteChange('signout')} className='near-white f3 link dim pa3 pointer pr4'> Sign Out</p>
+				</nav>
+			);
+		} else {
+			return (
+				<nav className='flex justify-end'>
+					<p onClick={() => onRouteChange('signin')} className='fw6 f3 link dim black pa3 pointer'> Sign In </p>
+					<p onClick={() => onRouteChange('register')} className='fw6 f3 link dim black pa3 pointer pr4'> Register </p>
+				</nav>
+			);
+		}
+}
+
+export default Navigation
