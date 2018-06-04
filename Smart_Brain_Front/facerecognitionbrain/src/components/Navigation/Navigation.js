69f9ad90cd0917797286854bdebca1e6f38f4ac7
Daniel Liao <39768672+dtliao@users.noreply.github.com>
2018-06-04 23:59:44 +0800

    adding face recognition project

---
+++ Smart_Brain_Front/facerecognitionbrain/src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+
+		if (isSignedIn) {
+			return (
+				<nav className='tr'>
+					<p onClick={() => onRouteChange('signout')} className='near-white f3 link dim underline pa3 pointer'> Sign Out</p>
+				</nav>
+			);
+		} else {
+			return (
+				<nav className='flex justify-end'>
+					<p onClick={() => onRouteChange('signin')} className='fw6 f3 link dim black underline pa3 pointer'> Sign In </p>
+					<p onClick={() => onRouteChange('register')} className='fw6 f3 link dim black underline pa3 pointer'> Register </p>
+				</nav>
+			);
+		}
+}
+
+export default Navigation
